3e54cfd1b1ec58d3b4e331e90c48e449a4b738e0
sandesh <sandeshkhadka217@gmail.com>
2023-02-24 18:29:41 +0545

    Add basic ui

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const resultDiv = document.querySelector(".result");
 let choice = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     let random = Math.floor(Math.random() * choice.length)
@@ -18,32 +19,36 @@ function decide(playerChoice, computerChoice) {
         if (computerChoice == "rock") return "computer";
         if (computerChoice == "paper") return "player";
     }
+    throw new Error("Unexpected choice");
 }
 
 function play() {
     let computerChoice = getComputerChoice();
-    let playerChoice = prompt("Enter your choice: ").toLowerCase();
+    let playerChoice = this.dataset['choice'];
     let winner = decide(playerChoice,computerChoice);
     console.log(`You: ${playerChoice} , Computer: ${computerChoice}`);
     switch (winner) {
         case "player":
             console.log(`You won ${playerChoice} beats ${computerChoice}`);
+            resultDiv.innerText = `You won ${playerChoice} beats ${computerChoice}`
             break;
         case "computer":
             console.log(`Computer won ${computerChoice} beats ${playerChoice}`);
+            resultDiv.innerText = `Computer won ${computerChoice} beats ${playerChoice}`
             break;
         case "tie":
             console.log(`It's a tie , You : ${playerChoice} Computer: ${computerChoice}`);
+            resultDiv.innerText = `It's a tie , You : ${playerChoice} Computer: ${computerChoice}`
             break;
 
         default:
             throw new Error("Unexpected decision");
     }
 }
-
-for(let i = 0; i < 5; i++){
-    play();
-}
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+choiceBtns.forEach(btn => btn
+    .addEventListener('click',play)
+);
 
 
 
